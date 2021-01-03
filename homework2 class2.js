0d142874935b262397a7fb0cd7e792c936eb0b69
biljana.radevska <b.radevska@yahoo.com>
2021-01-03 13:24:19 +0100

    homework2.class2.js

---
+++ homework2 class2.js
@@ -0,0 +1,49 @@
+let userInput=prompt("Enter year of birth");
+let numberInput=parseInt(userInput);
+
+let a=numberInput;
+let b=4;
+let c=0.12;
+let rezult=(numberInput-4)*0.12;
+console.log(rezult)
+
+
+if (rezult=0.0) {
+    console.log ("Rat")
+} 
+if (rezult=1.0){
+    console.log("Ox")
+}
+if (rezult=2) {
+    console.log("Tiger")
+}
+if (rezult=3) {
+    console.log("Rabbit")
+}
+if (rezult=4) {
+    console.log("Dragon")
+}
+if (rezult=5.0) {
+    console.log("Snake")
+}
+if (rezult=6) {
+    console.log ("Horse")
+}
+if (rezult=7) {
+    console.log ("Goat")
+}
+if (rezult=8) {
+    console.log ("Monkey")
+}
+if (rezult=9) {
+    console.log ("Rooster")
+}
+if (rezult=10){
+    console.log ("Dog")
+}
+if (rezult=11) {
+    console.log("Pig")
+}
+else {
+    alert("EROR")
+}
