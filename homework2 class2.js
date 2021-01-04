b0b44fde344b8685be38b947ac9178ed35619192
biljana.radevska <b.radevska@yahoo.com>
2021-01-04 09:32:44 +0100

    Change1

--- homework2 class2.js
+++ homework2 class2.js
@@ -3,50 +3,50 @@ let numberInput=parseInt(userInput);
 
 let a=numberInput;
 let b=4;
-let c=0.12;
-let rezult=(numberInput-4)*0.12
+let c=12;
+let rezult=(numberInput-4)%12
 console.log(rezult)
 
-if ((rezult>=-0.48)&& (rezult<=0.50)) {
+if (rezult=0) {
     console.log("Rat")
 }
-if ((rezult>=0.51)&& (rezult<=1.50)) {
+if (rezult=1) {
     console.log("Ox")
 }
-if ((rezult>=1.51)&& (rezult<=2.50)) {
+if  (rezult=2) {
     console.log("Tiger")
 }
-if ((rezult>=2.51)&& (rezult<=3.50)) {
+if (rezult=3) {
     console.log("Rabbit")
 }
-if ((rezult>=3.51)&& (rezult<=4.50)) {
+if (rezult=4) {
     console.log("Dragon")
 }
-if ((rezult>=4.51)&& (rezult<=5.50)) {
+if (rezult=5) {
     console.log("Snake")
 }
-if ((rezult>=5.51)&& (rezult<=6.50)) {
+if (rezult=6) {
     console.log("Horse")
 }
-if ((rezult>=6.51)&& (rezult<=7.50)) {
+if (rezult=7) {
     console.log("Goat")
 }
-if ((rezult>=7.51)&& (rezult<=8.50)) {
+if (rezult=8) {
     console.log("Monkey")
 
-}if ((rezult>=8.51)&& (rezult<=9.50)) {
+}if (rezul=9.50) {
     console.log("Roster")
 }
 
-if ((rezult>=9.51)&& (rezult<=9.50)) {
+if (rezult=10) {
     console.log("Dog")
 }
 
-if ((rezult>=9.51)&& (rezult<=10.50)) {
+if (rezult=11) {
     console.log("Pig")
 }
-if ((rezult<-0.48) || (rezult>10.50))
+if ((rezult<0) || (rezult>11)) {
     console.log("eror")
-
+}
 
 
