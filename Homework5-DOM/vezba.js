6d7e3a83cf07fa484a7b8369d81e23d5cf2e3a5f
biljana.radevska <b.radevska@yahoo.com>
2021-01-24 17:57:22 +0100

    Change24/01/2021

--- Homework5-DOM/vezba.js
+++ Homework5-DOM/vezba.js
@@ -8,7 +8,8 @@
 //Change the text of the header 1 in the last div
 //Change the text of the header 3 in the last div
 //Note:You can't change the HTML
-//niedno ne mi prifari
+
+//Niedno ne mi prifati!!!
 //1
 let firstDiv = document.querySelector("div");
 console.log(firstDiv);
