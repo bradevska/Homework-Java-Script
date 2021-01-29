a60e40c9b3ec27a84e3976fa97e1d9e1870c4540
biljana.radevska <b.radevska@yahoo.com>
2021-01-29 22:34:31 +0100

    Change 29.01.2021

---
+++ Homework6-Events/homework2.js
@@ -0,0 +1,50 @@
+//CREATE A DYNAMIC TABLE
+//Write a Javascript function that will dynamiclly create a table
+//User should input the number of Colums and Rows
+//In every table cell print which row and column it is (ex. Row-3 Column-1)
+
+
+
+function createTable()
+{
+let rn = prompt("Input number of rows");
+let cn = prompt("Input number of columns");
+  
+ for(var r=0;r<parseInt(rn,10);r++){
+   var x=document.getElementById('myTable').insertRow(r);
+
+   for(var c=0;c<parseInt(cn,10);c++) {
+     var y=  x.insertCell(c);
+     y.innerHTML="Row-"+r+" Column-"+c; 
+    }
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
