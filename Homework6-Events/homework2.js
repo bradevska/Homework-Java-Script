147e736e9a8ac66916855f5cac4c8caee61e81c8
biljana.radevska <b.radevska@yahoo.com>
2021-01-29 23:07:23 +0100

    Change 29.01.2021

--- Homework6-Events/homework2.js
+++ Homework6-Events/homework2.js
@@ -3,13 +3,11 @@
 //User should input the number of Colums and Rows
 //In every table cell print which row and column it is (ex. Row-3 Column-1)
 
-
-
-function createTable()
-{
 let rn = prompt("Input number of rows");
 let cn = prompt("Input number of columns");
-  
+
+function createTable(){
+
  for(var r=0;r<parseInt(rn,10);r++){
    var x=document.getElementById('myTable').insertRow(r);
 
