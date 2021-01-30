1711f34482937eaed10a15bc4b9127a2e302cb8f
biljana.radevska <b.radevska@yahoo.com>
2021-01-30 16:13:42 +0100

    change 31.01.2021

--- Vezbi class10/vezba2.js
+++ Vezbi class10/vezba2.js
@@ -5,13 +5,10 @@
 //- get the balance (this function should display the current balance of the user)
 //- exit (this function should close the window)
 
-let deposit=parseInt(prompt("Add money of users balance"));
-let withdrawal=parseint(prompt("Withdraw money of users balance"));
-let balance=parseint(prompt("Balance of usres"));
-let exit=parseint(prompt("Exit of ATM"));
-let currentBalance=1000;
+
 
 // Select what the user wishes to do
+var accountType = parseInt(prompt("Which type of account do you have? \n 1. deposit \n 2. withdraw \n 3. balance"));
 function selectType() {
     var accountType = parseInt(prompt("Which type of account do you have? \n 1. deposit \n 2. withdraw \n 3. balance"));
 	if (accountType !== "" && accountType !== null && !isNaN(accountType)) {
